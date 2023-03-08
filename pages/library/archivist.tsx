d2d16af7f83a74331b4dedc4501f1ef7d9c1f3cd
Sampriti <sampriti026@gmail.com>
2023-03-08 16:50:08 +0530

    update

--- pages/library/archivist.tsx
+++ pages/library/archivist.tsx
@@ -5,16 +5,17 @@ import Card from '~/library/ArticleCard'
 const Archivist: NextPage = () => {
   return (
     <Layout heading="Home" content="Homepage of the legal engineering guild.">
-      <Card title="Title. My Blog Post"
+      <Card
+        title="Title. My Blog Post"
         link="Link. https://myblog.com/post"
         notes="Notes. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget risus vitae elit mollis luctus. Nulla facilisi."
         summary="Summary. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget risus vitae elit mollis luctus. Nulla facilisi."
         date="2023-03-07"
         author="Author"
         type="Type"
-        topics={["Topic1", "Topic 2"]}
-        contributors={["Contributor1", "Contributor2"]}
-/>
+        topics={['Topic1', 'Topic 2']}
+        contributors={['Contributor1', 'Contributor2']}
+      />
     </Layout>
   )
 }
--- src/library/ArticleCard.tsx
+++ src/library/ArticleCard.tsx
@@ -1,31 +1,21 @@
-import React from "react";
+import React from 'react'
 
 interface CardProps {
-  title: string;
-  link: string;
-  notes: string;
-  summary: string;
-  date: string;
-  author: string;
-  type: string;
-  topics: string[];
-  contributors: string[];
+  title: string
+  link: string
+  notes: string
+  summary: string
+  date: string
+  author: string
+  type: string
+  topics: string[]
+  contributors: string[]
 }
 
-const Card: React.FC<CardProps> = ({
-  title,
-  link,
-  notes,
-  summary,
-  date,
-  author,
-  type,
-  topics,
-  contributors,
-}) => {
+const Card: React.FC<CardProps> = ({ title, link, notes, summary, date, author, type, topics, contributors }) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-6 mb-4">
-              <span className="text-gray-700 text-sm">{author}</span>
+      <span className="text-gray-700 text-sm">{author}</span>
       <h2 className="text-lg font-bold mb-2">{title}</h2>
       <div className="mb-4">
         <a href={link} target="_blank" rel="noopener noreferrer">
@@ -38,28 +28,20 @@ const Card: React.FC<CardProps> = ({
         <span className="text-gray-700 text-sm mr-2">{date}</span>
       </div>
       <div className="flex items-center mb-2">
-        <span className="bg-gray-300 text-gray-700 text-xs px-2 rounded-full mr-2">
-          {type}
-        </span>
+        <span className="bg-gray-300 text-gray-700 text-xs px-2 rounded-full mr-2">{type}</span>
         {topics.map((topic) => (
-          <span
-            key={topic}
-            className="bg-gray-300 text-gray-700 text-xs px-2 rounded-full mr-2"
-          >
+          <span key={topic} className="bg-gray-300 text-gray-700 text-xs px-2 rounded-full mr-2">
             {topic}
           </span>
         ))}
-                {contributors.map((contributor) => (
-          <span
-            key={contributor}
-            className="text-gray-700 text-sm"
-          >
+        {contributors.map((contributor) => (
+          <span key={contributor} className="text-gray-700 text-sm">
             {contributor}
           </span>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
